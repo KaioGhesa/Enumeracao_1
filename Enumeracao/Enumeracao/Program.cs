a3c99874721cbeec80c468e36b8e76058d6eb1e7
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-19 10:53:57 -0300

    Add files via upload

---
+++ Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,31 @@
+﻿using Enumeracao.Entites;
+using Enumeracao.Entites.Enums;
+
+
+namespace Enumeracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PedingPayment.ToString();
+            //Converter em string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+        }
+        }
+}
+
